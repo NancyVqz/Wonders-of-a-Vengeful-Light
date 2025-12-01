c242714ed1928db874d4423a0a036b949b02317d
Nancy_Home <polyvqz@gmail.com>
2025-12-01 15:35:06 -0600

    Video

--- Assets/2- SCRIPT/Transition/Audio Menu.cs
+++ Assets/2- SCRIPT/Transition/Audio Menu.cs
@@ -2,18 +2,8 @@ using UnityEngine;
 
 public class AudioMenu : MonoBehaviour
 {
-    [SerializeField] private Animator animIntro;
     void Start()
     {
         AudioManager.instance.Play("menu");
-
-        //if (GameManager.instance.introAppeared)
-        //{
-        //    return;
-        //}
-        //else
-        //{
-        //    animIntro.SetTrigger("intro");
-        //}
     }
 }
